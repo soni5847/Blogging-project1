44b883355629bfdbdf01a6cc8271a6f95a3089cd
huddamanish <mk2647566@gmail.com>
2022-09-05 17:40:11 +0530

    index file edited

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-console.log("project")
+const express = require('express');
+var bodyParser = require('body-parser');
+
+const route = require('./routes/route.js');
+const { default: mongoose } = require('mongoose');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb+srv://DipaliBohara:80761668@cluster0.4wyyohq.mongodb.net/dipaliProject1 "
+, {
+   useNewUrlParser: true 
+}
+).then( () => {console.log( "MongoDb is connected")}  )
+.catch( err => console.log(err))
+
+
+
+
+app.use('/', route);
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
+});
+
